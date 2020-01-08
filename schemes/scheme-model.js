12c5e3a489a9c76dd0d4df7ccd87adac3c8eb87c
Tara Sherman <taradelin@gmail.com>
2020-01-08 18:34:34 -0500

    Created GET request for all steps to a scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,7 +11,17 @@ function findById(id) {
 		.first();
 };
 
+function findSteps(id) {
+	return db('steps as st')
+		.join('schemes as s', 's.id', 'st.scheme_id')
+		.where({ scheme_id:id })
+		.select('st.id', 's.scheme_name', 'st.step_number', 'st.instructions')
+		.orderBy('st.step_number', 'asc')
+};
+
+
 module.exports = {
 	find,
-	findById
+	findById,
+	findSteps
 };
